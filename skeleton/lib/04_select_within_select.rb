bdff192706e77dc0874f67d3460232830864cf56
apodavonov3616 <apodavonov@gmail.com>
2022-04-26 16:59:25 -0400

    finished part 4

--- skeleton/lib/04_select_within_select.rb
+++ skeleton/lib/04_select_within_select.rb
@@ -35,6 +35,19 @@ end
 def larger_than_russia
   # List each country name where the population is larger than 'Russia'.
   execute(<<-SQL)
+  SELECT
+  name
+  FROM
+  countries
+  WHERE
+  population > (
+    SELECT
+    population
+    FROM
+    countries
+    WHERE
+    name = 'Russia'
+  )
   SQL
 end
 
@@ -42,6 +55,19 @@ def richer_than_england
   # Show the countries in Europe with a per capita GDP greater than
   # 'United Kingdom'.
   execute(<<-SQL)
+  select
+  name
+  from
+  countries
+  where
+  continent ='Europe' and gdp/population > (
+  select
+  gdp/population
+  from
+  countries
+  where
+  name = 'United Kingdom'
+  )
   SQL
 end
 
@@ -49,6 +75,19 @@ def neighbors_of_certain_b_countries
   # List the name and continent of countries in the continents containing
   # 'Belize', 'Belgium'.
   execute(<<-SQL)
+  select
+  name, continent
+  from
+  countries
+  where
+  continent in (
+    select
+    continent
+    from
+    countries
+    where
+    name in ('Belize', 'Belgium')
+  )
   SQL
 end
 
@@ -56,6 +95,25 @@ def population_constraint
   # Which country has a population that is more than Canada but less than
   # Poland? Show the name and the population.
   execute(<<-SQL)
+  select
+  name, population
+  from
+  countries
+  where
+  population > (
+    select
+  population
+  from
+  countries
+  where name = 'Canada'
+  ) and population <
+  (
+  select
+  population
+  from
+  countries
+  where name = 'Poland'
+  )
   SQL
 end
 
@@ -65,5 +123,18 @@ def sparse_continents
   # population.
   # Hint: Sometimes rewording the problem can help you see the solution.
   execute(<<-SQL)
+  select 
+  name, continent, population
+  from 
+  countries
+  where
+  continent not in (
+    select 
+    continent
+    from
+    countries
+    where
+    population >= 25000000
+  )
   SQL
 end
